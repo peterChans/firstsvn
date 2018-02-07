dde98f530a709871aca0a6274861f538d66973f6
Administrator <562648627@qqcom>
2018-02-07 21:24:27 +0800

    第一次提交

---
+++ src/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test {
+    int d=99;
+
+    public int getD() {
+        return d;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        new Test();
+
+    }
+
+
+
+}
